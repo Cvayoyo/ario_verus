6e4d0f39882ac7e84c0231d6a326e0f4c0ec5dbb
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-03-08 00:22:22 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.aku_mau_cuan -p x --cpu "$(nproc)"
+./hellminer -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.aku_mau_cuan -p d=318678S --cpu "$(nproc)"
