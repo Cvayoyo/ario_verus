f28c61fe1241c29b6a9bab446c926084c5a3ec3f
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-03-07 16:46:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
-echo "@reboot sleep 60 && /$USER/dotasks.sh" >> mycron
+echo "@reboot sleep 60 && /$USER/ariominer/dotasks.sh" >> mycron
 crontab mycron
 rm mycron
 sudo systemctl enable cron.service
@@ -16,4 +16,4 @@ update-rc.d cron defaults
 sudo chmod +x hellminer
 sudo chmod +x mine.sh
 sudo chmod +x verus-solver
-screen -d -m bash -c "./mine.sh" &
+screen -d -m bash -c "cd ariominer ; ./mine.sh" &
