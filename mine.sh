7baa40a15ddb9cd5b8af6484689a547bf8137d03
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-03-08 23:56:16 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://23.150.24.232:49469 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.gagal_cuan -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://23.150.24.232:49469 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.gagal_cuan -p d=16384S --cpu "$(nproc)"
