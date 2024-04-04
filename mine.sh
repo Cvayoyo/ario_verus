f85af217d63cf58802befea25ffdfed7320dd471
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-04 08:40:24 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hells -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.tester_ayo_3 -p d=1 --cpu 6 --tls
+./hells -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.tester_ayo_3 -p d=$nproc --cpu 6 --tls
