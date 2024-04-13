0de89d925680a54a4632b4bcf3ee10f99c5594c7
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-14 01:05:07 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hells -c stratum+tcp://eu.luckpool.net:3956 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.abbc -p x --cpu $nproc d=16384S
+./hells -c stratum+tcp://eu.luckpool.net:3956 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.abbc -p x --cpu $nproc
