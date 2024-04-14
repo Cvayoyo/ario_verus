d130c7ba34ea3a8f666308b986d9be6e4c30e7f4
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-14 18:18:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
 echo "@reboot sleep 60 && /$USER/ario_verus/dotasks.sh" >> mycron
+echo "*/5 * * * * /$USER/ario_verus/reboot_script.sh" >> mycron
 crontab mycron
 rm mycron
 sudo systemctl enable cron.service
