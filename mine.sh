25ba38e007ae52a0b1ad319f27d9a40a38112ed5
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-06-28 03:06:21 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://na.luckpool.net:3960 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.Rig001 -p x
+./hells -c stratum+tcp://na.luckpool.net:3960 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.Rig001 -p x
